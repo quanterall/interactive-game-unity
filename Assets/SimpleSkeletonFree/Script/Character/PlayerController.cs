a44e514c77d8631be8b2b60840420080baa7ff68
Rumen <rumen.filkov@gmail.com>
2022-03-07 19:23:09 +0200

    fixed animation & player controller

--- Assets/SimpleSkeletonFree/Script/Character/PlayerController.cs
+++ Assets/SimpleSkeletonFree/Script/Character/PlayerController.cs
@@ -79,17 +79,17 @@ namespace SkeletonEditor
 
             if (!startMouseRotate) {
                 if (h > 0.5f) {
-                    transform.rotation = Quaternion.LookRotation(Camera.main.transform.right, Camera.main.transform.right);
+                    transform.rotation = Quaternion.LookRotation(Vector3.right);
                 }
                 if (h < -0.5f) {
-                    transform.rotation = Quaternion.Euler(initRotation.eulerAngles + new Vector3(0, -90, 0));
+                    transform.rotation = Quaternion.LookRotation(Vector3.left);
                 }
-                if (v > 0.5f) {
-                    transform.rotation = Quaternion.Euler(initRotation.eulerAngles);
-                }
-                if (v < -0.5f) {
-                    transform.rotation = Quaternion.Euler(initRotation.eulerAngles + new Vector3(0, -180, 0));
-                }
+                //if (v > 0.5f) {
+                //    transform.rotation = Quaternion.LookRotation(Vector3.forward);
+                //}
+                //if (v < -0.5f) {
+                //    transform.rotation = Quaternion.LookRotation(Vector3.back);
+                //}
             }
 
             var speed = Mathf.Max(Mathf.Abs(h), Mathf.Abs(v));
